0f02d424623ee4f1acf7e3d84ea38f5f4b68f505
uurtech <kazdal.ugur@1ki3.com>
2018-04-29 13:03:45 +0300

    getParams updated

--- cool.js
+++ cool.js
@@ -1,12 +1,1 @@
-String.prototype.getParams = function(...args){
-    
-    let newUrl = new URL(this);
-    var result = {};
-    for(var i = 0; i < args.length; i++){
-        let searchparams = new URLSearchParams(this.search);
-        result[args[i]] = searchparams.get(args[i]);
-    }
-
-    return result;
-}
-
+String.prototype.getParams = function(...args){ let newUrl = new URL(this); var result = {}; for(var i = 0; i < args.length; i++){ let searchparams = new URLSearchParams(this.search); result[args[i]] = searchparams.get(args[i]); } return result; }
